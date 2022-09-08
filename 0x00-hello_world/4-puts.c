1d3839c0b8146d06977eabfc2a39615c458820bc
lakelexy <alao8380@yahoo.com>
2022-09-08 15:07:03 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - print the string in the put funtion
+ *
+ * Description: using the main function
+ * This program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main()
+{
+	char Mystr [50]:
+		Mystrcpy(Mystr, "Programming is like building a multilingual puzzle \n");
+	puts (Mystr);
+	return(0);
+}
