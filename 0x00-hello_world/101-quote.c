01b0dc676de97cee1e4809273722a160d828769d
lakelexy <alao8380@yahoo.com>
2022-09-08 17:58:14 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,10 @@
  *
  * Description: using the main function
  * This program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
- * Return (0)
+ * Return (1)
  */
 int main(void)
 {
-	sytem("echo 'and that piece of art is useful" - Dora Korpar, 2015-10-19'");
+	sytem("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
