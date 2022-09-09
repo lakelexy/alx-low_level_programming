bbff7c20900fd4a06eecb52001242525cbcb8ee7
lakelexy <alao8380@yahoo.com>
2022-09-08 18:49:58 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
-
-i/**
- * main - print the string using write functio *
+/**
+ * main - print the string without using the printf and puts function
+ *
  * Description: using the main function
- * This program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
- * Return (1)
+ * Return:returns 1
  */
-int write(const char *buf, unsigned int nbyte);
-int main(char** argv)
+int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-       return (1)	
-		
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1)
 }
+
+
