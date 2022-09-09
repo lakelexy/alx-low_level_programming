012a05b61d689b6d78bd5432f70d18581d0872c6
lakelexy <alao8380@yahoo.com>
2022-09-08 18:21:12 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,16 @@
 #include <string.h>
 #include <unistd.h>
 
-/**
- * main - print the string using write function
- *
+i/**
+ * main - print the string using write functio *
  * Description: using the main function
  * This program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
  * Return (1)
  */
-int main(void)
+int write(const char *buf, unsigned int nbyte);
+int main(char** argv)
 {
-	sytem("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+       return (1)	
+		
 }
