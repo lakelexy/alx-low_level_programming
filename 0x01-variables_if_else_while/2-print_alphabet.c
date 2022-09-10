04c24c6b529f9cd885ef899c728be19a6c5b61a0
lakelexy <alao8380@yahoo.com>
2022-09-09 21:23:43 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,12 @@
  * main - print the alphabet
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{	
 		putchar(alp[i]);
