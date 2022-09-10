c7fb6fae9705304102d17ebc6ae02660e4068855
lakelexy <alao8380@yahoo.com>
2022-09-10 10:10:43 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,8 @@ int main(void)
 	char low, e, q;
 	e = 'e' ;
 	q = 'q' ;
-	for (low = 'a' ; low <= 'z' ; low++)
+
+	for (low = 'a' ;low <= 'z' ;low++)
 	{
 		if (low != e && low != q)
 			putchar(low);
