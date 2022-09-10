08345432f2be33f9f85837627ec464407ae324fd
lakelexy <alao8380@yahoo.com>
2022-09-09 19:42:05 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,9 @@
  * Description: Using the main function
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
