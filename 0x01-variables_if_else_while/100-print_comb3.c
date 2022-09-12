85d6e72c4bf2ddf5f79890493c20815f953bfcda
lakelexy <alao8380@yahoo.com>
2022-09-11 18:53:37 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 8; digit1++)
+	{
+		for (digit2 = 1; digit2 < 9; digit2++)
+		{
+		putchar((digit1 % 8) + '0');
+		putchar((digit2 % 9) + '1');
+		if (digit1 == 7 && digit2 == 8)
+			continue;
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
