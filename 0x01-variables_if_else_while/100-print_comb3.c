81ee4b95e3046b243956b26e475d6f603e3c2808
lakelexy <alao8380@yahoo.com>
2022-09-12 02:51:32 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  * Return: Always 0 (succes)
  */
 int main(void)
 {
-	int digit1, digit2;
+	int n;
+	int j;
 
-	for (digit1 = 0; digit1 < 7; digit1++)
+	for (n = '0'; n < '9'; n++)
 	{
-		for (digit2 = 1; digit2 < 8; digit2++)
+		for (j = n + 1; j <= '9'; j++)
 		{
-		putchar((digit1 % 8) + '0');
-		putchar((digit2 % 9) + '1');
-		if (digit1 == 7 && digit2 == 8)
-			continue;
-		putchar(',');
-		putchar(' ');
+			putchar(n);
+			putchar(j);
+			if (!(n == '8' && j == '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
 	return (0);
-}
+}	
