a2150b6b66d9c79fc08e812456c4c3521bf63730
lakelexy <alao8380@yahoo.com>
2022-09-13 12:26:18 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
-/**
+/**  ( 
  * main - check the code
  *
  * Return: Always 0 (success)
  */
-int _putchar(char c);
+void print_alphabet(void);
 {
-
+	
 	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			putchar(ch);
-		}
+	{
+		putchar(ch);
+	}
 putchar('\n');
-return (0);
+return(0);
 }
