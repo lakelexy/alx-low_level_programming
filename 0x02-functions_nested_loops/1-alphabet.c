de38c2539dc9dbb6aa7b40fa2616253ecd0ad8f5
lakelexy <alao8380@yahoo.com>
2022-09-13 11:46:55 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (success)
+ */
+int _putchar;
+{
+
+	char alp[26] = "abcdefghijklmnopqrst";
+	int i;
+	for (i = 0; i < 26; i++)
+		{
+			putchar(alp[i])
+		}
+putchar('\n');
+return (0);
+}
