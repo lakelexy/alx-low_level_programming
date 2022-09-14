ad6572da3f8b31880092c57027a83eb2a706f014
lakelexy <alao8380@yahoo.com>
2022-09-14 01:50:12 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**   
- * print_alphabet- prints the alphabet in lowercase
+ * print_alphabet- function to prints the alphabet in lowercase
  *
- * Return: Always 0 (success)
- * @void: lorem ipsum
- * Description: this function prints all the alphabet
+ * Return: void
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char a = 'a';
 	int i;
