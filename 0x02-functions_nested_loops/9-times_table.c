3943f6ea6be6da63e7bd75fac93a5bcc580640d3
lakelexy <alao8380@yahoo.com>
2022-09-14 17:46:47 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,12 +7,11 @@ void times_table(void)
 {
 	int num;
 	int mult;
-	int prod;
 
 	for (num = 0; num <= 9; num++)
 	{
 		for (mult = 0; mult <= 9; mult++)
-			prod = num * mult
+			prod = num * mult;
 				if (mult == 0)
 				{
 					_putchar('0');
