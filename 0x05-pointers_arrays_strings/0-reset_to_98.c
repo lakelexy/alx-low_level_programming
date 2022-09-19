c0d3324e775ee38c4c9df4e28b0c2e47175a7e75
lakelexy <alao8380@yahoo.com>
2022-09-19 06:32:28 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - derefencing pointers
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int *p;
+
+	 n = 402;
+	 p = &n;
+	 printf("n=%d\n", n);
+	 *p = 98;
+	 printf("n=%d\n", n);
+	 return (0);
+}
