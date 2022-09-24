96b1b21a8337706403ad6cf81e478ebd0e226221
lakelexy <alao8380@yahoo.com>
2022-09-24 05:13:19 -0700

    task8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,22 +8,28 @@
 
 char *rot13(char *s)
 {
-	int count = 0, i;
-	char alphabeth[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int stringCount, rotation;
+	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z'};
+	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M'};
 
-	while (*(s + count) != '\0')
+	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
 	{
-		for (i = 0; i < 52; i++)
+		for (rotation = 0; rotation < 53; rotation++)
 		{
-			if (*(s + count) == alphabeth[i])
+			if (r1[rotation] == s[stringCount])
 			{
-				*(s + count) = rot13[i];
+				s[stringCount] = r2[rotation];
 				break;
 			}
 		}
-		count++
 	}
-
 	return (s);
 }
