5d9bb8b3f996c438b8708eb55b9d4033c826ab47
lakelexy <alao8380@yahoo.com>
2022-09-24 06:18:07 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,37 +2,33 @@
 
 /**
  * cap_string - capitalizes all every first letter of a word in a string
- * seperators of words are: space, tabulation,
- * new line, ,, ;, ., !, ?, ", (, ), {, and }.
- * @s: pointer to string
+ * @s: string to modify
  *
- * Return: pointer to string
+ * Return: the resulting string
  */
-
 char *cap_string(char *s)
 {
-	int count;
+	int i, j;
 
-	/* scan through string */
-	count = 0;
-	while (s[count] != '\0')
-	{/* if next character after countis a char , capitalise it */
-		if (s[0] >= 97 && s[0] <= 122)
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
 		{
-			s[0] = s[0] - 32;
-		}
-		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-				|| s[count] == ',' || s[count] == ';' || s[count] == '.'
-				|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
-				|| s[count] == '{' || s[count] == '}')
-		{
-			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			if (s[i] == spe[j])
 			{
-				s[count + 1] = s[count + 1] - 32;
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
 			}
 		}
-		count++
 	}
+
 	return (s);
 }
